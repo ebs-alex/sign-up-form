c17ebee09a4dda055121c7772286cfbcd5666480
Alex Moran <ebs.alex@icloud.com>
2023-11-20 10:31:34 -0800

    added JS verification of confirm password field. use js to enable submit button once pwds match

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("test")
+let submitBtn = document.querySelector("#submitBtn")
+let pwd = document.querySelector("#pwd")
+let pwd_confirm = document.querySelector("#pwd-confirm")
+
+
+pwd_confirm.addEventListener("keyup", compareEntries)
+
+
+function compareEntries() {
+
+    if (pwd.value == pwd_confirm.value) {
+        pwd_confirm.style.outline = "none"
+        pwd_confirm.style.border = "2px solid greenyellow"
+        console.log("matched")
+        submitBtn.disabled = false
+    } else  {
+        console.log("NO match")
+        pwd_confirm.style.border = "2px solid red"
+        submitBtn.disabled = true
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// console.log(pwd)
